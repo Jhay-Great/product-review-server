36bcf2ef82f0b55f4901f826cc3c4f11c7209fe5
Jhay-Great <alemonai.jenef@gmail.com>
2025-08-30 23:13:43 +0000

    fix(cors): corrects cors implementation and potential bug

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
-import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 
 import router from './routes';
@@ -8,9 +7,6 @@ import corsConfig from './config/corsConfig';
 const app = express();
 
 app.use(corsConfig);
-// app.use(cors({
-//     origin: 'http://localhost:4200',
-// }));
 
 app.use(express.json());
 
@@ -33,11 +29,5 @@ app.use((err:Error, req:Request, res:Response, next:NextFunction) => {
         message: 'Server error',
     })
 });
-// app.use((err, req, res, next) => {
-//   if (err.message === 'Not allowed by CORS') {
-//     return res.status(403).json({ error: 'CORS Error: Origin not allowed' });
-//   }
-//   next(err);
-// });
 
 export default app;
--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -2,20 +2,14 @@ import cors from 'cors';
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || [];
 
-// const corsConfig = cors({});
 const corsConfig = cors({
     origin(requestOrigin, callback) {
-        console.log('request origin: ', requestOrigin);
         if (!requestOrigin) {
-            // console.log('request has no origin', requestOrigin)
             callback(null, true);
-        };
-
-        if (requestOrigin && allowedOrigins.includes(requestOrigin)) {
+        } else if (requestOrigin && allowedOrigins.includes(requestOrigin)) {
             callback(null, true);
         } else {
-            // console.log('else block also runs');
-            // callback( new Error('Not allowed by CORS'))
+            callback( new Error('Not allowed by CORS'))
         }
     }
 });
