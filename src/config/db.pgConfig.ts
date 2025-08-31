50933c3e3bb02096c13c629e2b971463aeaa8c64
Jhay-Great <alemonai.jenef@gmail.com>
2025-08-31 00:05:35 +0000

    refactor: minor change to port implementation

--- src/config/db.pgConfig.ts
+++ src/config/db.pgConfig.ts
@@ -5,8 +5,7 @@ const pool = new Pool({
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
     host: process.env.PG_HOST,
-    port: 5432,
-    // connectionString: process.env.CONNECTION_STRING,
+    port: parseInt(process.env.PG_PORT || '5432'), 
 });
 
 export const checkDBServer = async function() {
