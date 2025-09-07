2960e5fe911ef7b1dfbe94e2ef50ccae68c426f8
Jhay-Great <alemonai.jenef@gmail.com>
2025-09-07 11:19:57 +0000

    chore: add jwt and bcrypt packages

---
+++ src/utils/harsh.ts
@@ -0,0 +1,10 @@
+// src/utils/hash.ts
+import bcrypt from 'bcrypt';
+
+export const hashPassword = (password: string): Promise<string> => {
+  return bcrypt.hash(password, 10);
+};
+
+export const comparePassword = (password: string, hash: string): Promise<boolean> => {
+  return bcrypt.compare(password, hash);
+};
